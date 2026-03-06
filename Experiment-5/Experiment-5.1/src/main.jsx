6582a07fed1f2a73e455b4ca325952777ed02fb5
yasha07-ai <tasaneemyasha.com>
2026-03-06 10:29:16 +0530

    Added new files

--- Experiment-5/Experiment-5.1/src/main.jsx
+++ Experiment-5/Experiment-5.1/src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client.js'
 import './index.css'
 import App from './App.jsx'
 
